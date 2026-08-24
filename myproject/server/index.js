c9f5515b1dd46f0f808799f22015c69547fa7664
Ha Thang <thanghak25@gmail.com>
2026-08-24 18:16:59 +0700

    update

--- myproject/server/index.js
+++ myproject/server/index.js
@@ -8,16 +8,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-app.get(['/', '/api/get'], (req, res) => {
-=======
 app.get('/api/data', (req, res) => {
->>>>>>> 607d3f0071e1c96a12ffb4d3e42d576d7f4b0255
     res.json({
         message: 'Hello from the server! 123123123',
     });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
