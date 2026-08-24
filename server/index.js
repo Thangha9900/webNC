4b1dd2641d0ffa9fa518efdee545abc1b77caeb2
HaThang <151729861+Thangha9900@users.noreply.github.com>
2026-08-24 08:55:47 +0000

    cap nhat

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
-// app.use(express.json());
+// app.use(express.json())  ;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
