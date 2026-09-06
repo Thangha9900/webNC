2aa1a286786f161271e93c441236f9f85e9e6358
Ha Thang <thanghak25@gmail.com>
2026-09-06 21:24:48 +0700

    update file

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,7 @@ function App() {
   const [message, setMessage] = useState("Đang kết nối server...");
 
   useEffect(() => {
-<<<<<<< HEAD
     fetch("/api/data")
-=======
-    fetch("https://xxxxx-5000.app.github.dev/api/data")
->>>>>>> 607d3f0071e1c96a12ffb4d3e42d576d7f4b0255
       .then((response) => response.json())
       .then((data) => {
         setMessage(data.message);
@@ -19,12 +15,7 @@ function App() {
       });
   }, []);
 
-  return (
-    <div>
-      <h1>WebNC</h1>
-      <h2>Client React</h2>
-      <p>{message}</p>
-    </div>
+  return (<div> <h1>WebNC</h1> <h2>Client React</h2> <p>{message}</p> </div>
   );
 }
 
